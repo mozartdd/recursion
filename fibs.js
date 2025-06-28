e60a77a4e1552db9456acbdd6fd8a55f3e05350a
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-28 13:22:36 +0100

    Added recursion version of fibonacci sequance function

--- fibs.js
+++ fibs.js
@@ -1,9 +1,25 @@
+const fibonacci = [0, 1, 1];
+
 function fibs(n) {
-  const fibonacci = [0, 1, 1];
-  for (let i = 3; i < n; i++) {
+  if (n < 0) {
+    return 'Fibonacci number can not be negative number.';
+  } else if (n === 0) {
+    return fibonacci[0];
+  } else if (n === 1 || n === 2) {
+    return fibonacci[1];
+  } else {
+    for (let i = fibonacci.length; i < n; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+    }
+    return fibonacci;
   }
-  return fibonacci;
 }
 
-console.log(fibs(21));
+function fibsRec(n) {
+  if (n === 0 ) return 0;
+  else if (n === 1 || n === 2) return 1;
+  return fibonacci[n] = fibsRec(n-1) + fibsRec(n-2);
+}
+
+fibsRec(44);
+console.log(fibonacci);
