52ac5f512ce27713e06ffca886003323cc9a57d5
Dainis Dilevka <thedenisovan@proton.me>
2025-06-29 18:04:43 +0100

    Added bubble sort algorithm

---
+++ sort/bubbleSort.js
@@ -0,0 +1,16 @@
+function sortArray(array) {
+  let newArray = array.slice();
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = i + 1; j < newArray.length; j++) {
+      if (newArray[i] <= newArray[j]) {
+        continue;
+      }
+      if (newArray[i] > newArray[j]) {
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+      }
+    }
+  }
+  return newArray;
+}
+
+console.log(sortArray([22, 5, 21, 4, 5]));
